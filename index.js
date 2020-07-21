c21143bea15e0ca3fc7cea2f02191ad6854a2582
denimartn <43113570+denimartn@users.noreply.github.com>
2020-07-21 16:57:25 +0200

    complete a task

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-// I can add a new task
-
-//when click add, call this function
 function onInit() {
   document.querySelector("#add").addEventListener("click", function (event) {
     event.preventDefault();
     renderTodo();
   });
+}
 
-  function renderTodo() {
-    //save input in var
-    let input = document.querySelector("input").value;
+function renderTodo() {
+  //save input in var
+  let input = document.querySelector("input").value;
 
-    //create a proto of the todo
-    let todo = {
-      id: Math.random(),
-      value: input,
-      isDone: false,
-    };
+  //create a proto of the todo
+  let todo = {
+    id: Math.random(),
+    value: input,
+    isDone: false,
+  };
 
-    //create a li el
-    let li = document.createElement("li");
+  //create a li el
+  let li = document.createElement("li");
 
-    //set li inner text
-    li.innerText = todo.value;
+  //set li inner text
+  li.innerText = todo.value;
 
-    //create a span
-    let span = document.createElement("span");
+  //create a span
+  let span = document.createElement("span");
 
-    //append span to li
-    li.append(span);
+  //append span to li
+  li.append(span);
 
-    //create a checkbox
-    let checkbox = document.createElement("input");
+  //create a checkbox
+  let checkbox = document.createElement("input");
 
-    //set type of input
-    checkbox.type = "checkbox";
+  //add click event to checkbox
+  checkbox.addEventListener("click", function () {
+    //change todo state
+    todo.isDone = true;
+  });
 
-    //append checkbox to li
-    li.append(checkbox);
+  //set type of input
+  checkbox.type = "checkbox";
 
-    //select ul
-    let ul = document.querySelector("ul");
+  //append checkbox to li
+  li.append(checkbox);
 
-    //append li to ul
-    ul.append(li);
-  }
+  //select ul
+  let ul = document.querySelector("ul");
+
+  //append li to ul
+  ul.append(li);
 }
 
 onInit();
