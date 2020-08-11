ebe986e5cfefbdc9f63718bfc857b1f1c3e82518
denimartn <43113570+denimartn@users.noreply.github.com>
2020-08-11 16:06:11 +0200

    add comments

--- index.js
+++ index.js
@@ -1,68 +1,97 @@
 let all = [];
 let currentView = "all";
-function onInit() {
-  getTodos();
-  renderTodo(all);
+let deleteAllbutton = document.querySelector(".delete-all");
+let allButton = document.querySelector("#all");
+let activeButton = document.querySelector("#active");
+let completedButton = document.querySelector("#completed");
+let inputWrapper = document.querySelector(".input-wrapper");
 
-  document.querySelector(".delete-all").style.display = "none";
+function onInit() {
+  getTodosFromLocalstorage();
+  // render saved todos after initialization
+  renderTodos(all);
+
+  // hide delete all button
+  deleteAllbutton.classList.add("hide");
+  // add click event on add button
   document.querySelector("#add").addEventListener("click", function (event) {
     event.preventDefault();
+    // create a new todo after every submit
     makeTodo();
-
+    //change rendering
     if (currentView === "active") {
       let active = all.filter((todo) => !todo.isDone);
-      renderTodo(active);
+      renderTodos(active);
     }
-
+    //reset input
     document.querySelector("input").value = "";
   });
 
-  document.querySelector("#all").addEventListener("click", function () {
+  allButton.addEventListener("click", function () {
+    //update the current view
     currentView = "all";
-    document.querySelector("#all").classList.add("active");
-    document.querySelector("#active").classList.remove("active");
-    document.querySelector("#completed").classList.remove("active");
-    renderTodo(all);
-    document.querySelector(".input-wrapper").style.display = "block";
-    document.querySelector(".delete-all").style.display = "none";
+    //dynamically change style after clicking on buttons
+    allButton.classList.add("active");
+    activeButton.classList.remove("active");
+    completedButton.classList.remove("active");
+    //render all todos
+    renderTodos(all);
+    //show input button
+    inputWrapper.classList.add("show");
+    //hide delete-all button
+    deleteAllbutton.classList.add("hide");
   });
 
   document.querySelector("#completed").addEventListener("click", function () {
+    //update the current view
     currentView = "completed";
-    document.querySelector("#all").classList.remove("active");
-    document.querySelector("#active").classList.remove("active");
-    document.querySelector("#completed").classList.add("active");
+    //dynamically change style after clicking on buttons
+    allButton.classList.remove("active");
+    activeButton.classList.remove("active");
+    completedButton.classList.add("active");
+    //get completed todos
     let completed = all.filter((todo) => todo.isDone);
-    renderTodo(completed);
-    document.querySelector(".input-wrapper").style.display = "none";
-    document.querySelector(".delete-all").style.display = "block";
+    //render completed todos
+    renderTodos(completed);
+    //show input button
+    inputWrapper.classList.add("hide");
+    //hide delete-all button
+    deleteAllbutton.classList.add("show");
   });
 
-  document.querySelector("#active").addEventListener("click", function () {
+  activeButton.addEventListener("click", function () {
+    //update the current view
     currentView = "active";
-    document.querySelector("#all").classList.remove("active");
-    document.querySelector("#active").classList.add("active");
-    document.querySelector("#completed").classList.remove("active");
+    //dynamically change css after clicking on buttons
+    allButton.classList.remove("active");
+    activeButton.classList.add("active");
+    completedButton.classList.remove("active");
+    //get active todos
     let active = all.filter((todo) => !todo.isDone);
-    renderTodo(active);
-    document.querySelector(".input-wrapper").style.display = "block";
-    document.querySelector(".delete-all").style.display = "none";
+    //render active todos
+    renderTodos(active);
+    //show input button
+    inputWrapper.classList.add("show");
+    //hide delete-all button
+    deleteAllbutton.classList.add("hide");
   });
 }
 
-function renderTodo(arr) {
-  let ul = document.querySelector("ul");
+function renderTodos(arr) {
+  //select the list
+  let list = document.querySelector("ul");
 
-  ul.innerText = "";
+  list.innerText = "";
 
   for (let todo of arr) {
-    let li = document.createElement("li");
+    let item = document.createElement("li");
     let span = document.createElement("span");
     span.innerText = todo.value;
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    li.append(checkbox);
-    li.append(span);
+    item.append(checkbox);
+    item.append(span);
+    //after click disable checkbox and update todo as done
     checkbox.addEventListener("click", function () {
       todo.isDone = true;
       checkbox.disabled = true;
@@ -72,46 +101,53 @@ function renderTodo(arr) {
       checkbox.disabled = true;
       checkbox.checked = true;
     }
-
-    ul.append(li);
-
+    //appen todo to list
+    list.append(item);
+    //check if the current view is completed
     if (currentView === "completed") {
       const span = document.createElement("span");
       span.classList.add("delete-span");
+      //create a button to delete one todo
       const deleteOne = document.createElement("button");
       deleteOne.type = "button";
       deleteOne.innerText = "X";
       span.append(deleteOne);
       deleteOne.classList.add("delete-one");
-      li.append(span);
+      item.append(span);
+      //get not deleted todos
       deleteOne.addEventListener("click", function () {
         all = all.filter((item) => {
           return item.id !== todo.id;
         });
-        ul.removeChild(li);
+        list.removeChild(item);
       });
     }
 
-    document
-      .querySelector(".delete-all")
+    deleteAllbutton
+      //get completed todos
       .addEventListener("click", function () {
         completed = all.filter((item) => {
           return item.isDone;
         });
+        //clear the completed list
         completed = [];
+        //update all array
         all = all.filter((item) => !item.isDone);
-        ul.innerText = "";
+
+        list.innerText = "";
       });
   }
 }
 
 function makeTodo() {
+  //get input value
   let input = document.querySelector("input").value;
 
+  //check if input is empty
   if (input.length === 0) {
     return;
   }
-
+  //create a proto of the todo
   let todo = {
     id: Math.random(),
     value: input,
@@ -120,15 +156,15 @@ function makeTodo() {
 
   all.push(todo);
 
-  renderTodo(all);
+  renderTodos(all);
 }
 
-function saveTodos() {
+function saveTodosOnLocalstorage() {
   let str = JSON.stringify(all);
   localStorage.setItem("todos", str);
 }
 
-function getTodos() {
+function getTodosFromLocalstorage() {
   let str = localStorage.getItem("todos");
   all = JSON.parse(str);
   if (!all) {
@@ -137,6 +173,7 @@ function getTodos() {
 }
 
 onInit();
+
 window.addEventListener("beforeunload", function () {
-  saveTodos();
+  saveTodosOnLocalstorage();
 });
